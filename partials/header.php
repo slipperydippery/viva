e311638c38088ed3d31e391d6e51f484c5678526
Maarten de Jager <maartendejager@gmail.com>
2018-11-30 14:56:23 +0100

    localize viva css

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head itemscope itemtype="https://schema.org/WebSite">
-	<link rel="stylesheet" href="https://www.vivazorggroep.nl/css/20694426/e2967677dc54ad387d485fbad1231bce.css" type="text/css"/>
+
+  <link rel="stylesheet" href="code/css/vivastyle.css" type="text/css"/>
 	<link rel="stylesheet" href="code/css/wwstyle.css" type="text/css"/>
     <script type="text/javascript" src="code/js/jquery-2.2.1.js"></script>
 	<script type="text/javascript" src="code/js/app.js"></script>
